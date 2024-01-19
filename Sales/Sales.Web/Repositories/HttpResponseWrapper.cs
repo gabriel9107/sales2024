4dec5b07aedc6bf01aed66ebe7bb79fe69002fc7
Gabriel Montero <gabriel9107@gmail.com>
2024-01-19 15:31:53 -0400

    repositorio

---
+++ Sales/Sales.Web/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,52 @@
+﻿using System.Net
+
+namespace Sales.Web.Repositories
+{
+    public class HttpResponseWrapper<T>
+    {
+
+
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMenssage)
+        {
+            Error = error;
+            Response = response;
+            HttpResponseMessage = httpResponseMenssage;
+
+
+
+        }
+
+        public bool Error { get; set; }
+        public T? Response { get; set; }
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        public async Task<string?> GetErrorMenssage()
+        {
+            if(!Error)
+            {
+                return null;
+            }
+
+            var codigoEstatus = HttpResponseMessage.StatusCode;
+            if(codigoEstatus == HttpStatusCode.NotFound )
+            {
+                return "Recurso no encontrado";
+            }
+            else if(codigoEstatus == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            else if( codigoEstatus == HttpStatusCode.Unauthorized )
+            {
+                return "Tienes que loquarte para hacer esta operacion";
+            }
+            else if(codigoEstatus == HttpStatusCode.Forbidden )
+            {
+                return "No tienes permisos para hacer esta operacion";
+            }
+
+            return "Ha ocurrido un error inesperado";
+        }
+
+    }
+}
---
+++ Sales/Sales.Web/Repositories/Repository.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Sales.Web.Repositories
+{
+    public class Repository : IRepository
+    {
+        public Task<HttpResponseWrapper<T>> Get<T>(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseWrapper<object>> Post<T>(string url, T model)
+        {
+         
+        }
+
+        public Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
